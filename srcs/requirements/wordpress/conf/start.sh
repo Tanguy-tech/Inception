5f076848aec29e7ae48af90b304794981e8da9e5
Tanguy Billon <tanguy_billon@hotmail.fr>
2021-10-08 12:32:45 +0200

    add env variables

--- srcs/requirements/wordpress/conf/start.sh
+++ srcs/requirements/wordpress/conf/start.sh
@@ -6,7 +6,7 @@
 #    By: tanguy <tanguy@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/10/08 12:02:22 by tanguy            #+#    #+#              #
-#    Updated: 2021/10/08 12:04:07 by tanguy           ###   ########.fr        #
+#    Updated: 2021/10/08 12:32:16 by tanguy           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,8 +14,8 @@ mkdir /var/www/wordpress
 cd /var/www/wordpress
 mv /wp-config.php ./
 wp core download --allow-root
-wp core install --url="https://tbillon.42.fr" --title="INCEPTION/ $1" --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
-wp user create $USER_NAME $USER_EMAIL --user_pass=$USER_PASSWORD --role=editor --allow-root
+wp core install --url="https://tbillon.42.fr" --title="INCEPTION/ $1" --admin_user=$ADMIN_USR --admin_password=$ADMIN_PSWD --admin_email=$ADMIN_EMAIL --allow-root
+wp user create $USER_NAME $USER_EMAIL --user_pass=$USER_PSWD --role=editor --allow-root
 
 chown www-data:www-data /var/www
 chmod 744 /var/www
