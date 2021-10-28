650e432108c5d7e8fc94e9cc0cb825e2419f6e1b
Tanguy Billon <tbillon@z3r2p1.42lyon.fr>
2021-10-28 11:47:54 +0200

    update

--- srcs/requirements/mariadb/conf/install_db.sh
+++ srcs/requirements/mariadb/conf/install_db.sh
@@ -3,22 +3,19 @@
 #                                                         :::      ::::::::    #
 #    install_db.sh                                      :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: tanguy <tanguy@student.42.fr>              +#+  +:+       +#+         #
+#    By: tbillon <tbillon@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/10/08 11:06:20 by tanguy            #+#    #+#              #
-#    Updated: 2021/10/14 11:36:12 by tanguy           ###   ########.fr        #
+#    Updated: 2021/10/28 10:44:08 by tbillon          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-#!/bin/sh
+service start mysql
 
-until mysql
-do
-	echo "NO_UP"
-done
+echo "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE User = 'root';" | mysql -u root
+echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root');" | mysql -u root -proot
+echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DB} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci" | mysql -u root -proot
+echo "GRANT ALL PRIVILEGES ON ${MYSQL_DB} .* TO '${MYSQL_USR}'@'%' IDENTIFIED BY '${MYSQL_PSWD}';" | mysql -u root -proot
+echo "FLUSH PRIVILEGES;" | mysql -u root -proot
 
-mysql -e "CREATE USER '${MYSQL_USR}'@'localhost' IDENTIFIED BY '${MYSQL_PSWD}';"
-mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DB};"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USR}'@'%' IDENTIFIED BY '${MYSQL_PSWD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DB < /base.sql
+exec mysqld -u root -proot --datadir="/var/lib/mysql"
